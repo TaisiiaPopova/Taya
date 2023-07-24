17570248300b658d20fe1abe4ba07da2acb32a7a
Taisiia Popova <138374882+TaisiiaPopova@users.noreply.github.com>
2023-07-24 15:31:29 +0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.controller.dto.User;
+import com.example.demo.exception.DuplicateUserException;
+import com.example.demo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final Converter<User, com.example.demo.repository.model.User> converter;
+
+    public UserService(UserRepository userRepository, Converter<User, com.example.demo.repository.model.User> converter) {
+        this.userRepository = userRepository;
+        this.converter = converter;
+    }
+
+    public int save(User user) throws DuplicateUserException {
+        if (userRepository.existsByFirstNameAndLastName(user.getName(), user.getLastName())) {
+            throw new DuplicateUserException(user);
+        }
+        return userRepository.save(converter.toEntity(user)).getId();
+    }
+
+    public int count() {
+        return userRepository.count();
+    }
+
+    public User findById(int id) {
+        return converter.toDto(userRepository.findById(id));
+    }
+
+    public Collection<User> findAll() {
+        return Collections.emptyList();
+    }
+}
