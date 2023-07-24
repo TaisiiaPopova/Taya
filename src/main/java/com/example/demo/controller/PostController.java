06013d75a112f1652adda0dda80e8457101df284
Taisiia Popova <138374882+TaisiiaPopova@users.noreply.github.com>
2023-07-24 17:53:41 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import com.example.demo.exception.UnknownPostException;
+import com.example.demo.exception.UnknownUserException;
+import com.example.demo.service.PostService;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.controller.dto.Post;
+
+import java.util.List;
+
+@RestController
+public class PostController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping ("/api/posts/new")
+    public int createPost(@RequestBody Post post) throws UnknownUserException
+    {
+        return postService.save(post).getUserId();
+    }
+
+    @GetMapping("/{userId}/posts")
+    public List<com.example.demo.controller.dto.Post> getPostsUserId(@PathVariable int userId) throws UnknownUserException
+    {
+        return postService.getPostsByUserId(userId);
+    }
+
+    @DeleteMapping("/api/posts/delete/{id}")
+    public void deletePost(@PathVariable int id) throws UnknownPostException
+    {
+        postService.delete(id);
+    }
+
+    @PutMapping ("/api/posts/edit/{id}")
+    public int editPost(@RequestBody Post post, @PathVariable int id) throws UnknownPostException
+    {
+        return postService.edit(id, post);
+    }
+}
