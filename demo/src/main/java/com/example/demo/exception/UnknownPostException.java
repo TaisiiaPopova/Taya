6c7a279dd9d7c8cdde4f24fc3bf37dd1a9988b70
Taisiia Popova <138374882+TaisiiaPopova@users.noreply.github.com>
2023-07-24 17:50:03 +0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/exception/UnknownPostException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class UnknownPostException extends Exception{
-    public UnknownPostException()
-    {
-        super("Post doesn't exist.");
-    }
-}
